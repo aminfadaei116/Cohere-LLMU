94ddbf8d60d512da21036d622f5835b09c865d7d
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-08-07 21:00:23 -0400

    updated the load file

--- models/utils/utils.py
+++ models/utils/utils.py
@@ -6,7 +6,7 @@ def load_api_key():
     load my cohere API key
     :return: str
     """
-    with open('config.yaml', 'r') as config_file:
+    with open('api_key.yml', 'r') as config_file:
         config = yaml.safe_load(config_file)
         api_key = config['api_key']
     return api_key
--- playground.py
+++ playground.py
@@ -5,7 +5,16 @@ from models.utils.utils import load_api_key
 def main():
     api_key = load_api_key()
     co = cohere.Client(api_key)
-    print(co)
+    prompt = "Hello World is a program that"
+
+    response = co.generate(
+        model='xlarge',
+        prompt=prompt,
+        max_tokens=75,
+        temperature=0.4)
+
+    output = response.generations[0].text
+    print(output)
 
 
 if __name__ == "__main__":
