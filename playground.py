42683f0e60c682e02779c42aa7d52617c231e0c2
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-08-10 23:44:24 -0400

    feat: Check the api_key before running the codes

--- playground.py
+++ playground.py
@@ -1,14 +1,17 @@
 import cohere
 from models.utils.utils import *
+from models.utils.generate_text import generate_rest_of_the_text
+from models.utils.classify import classify
 
 
 def main():
     api_key = load_api_key()
     co = cohere.Client(api_key)
 
+    assert co.check_api_key()["valid"], "The api_key is not valid!"
+
     # prompt = "The best way to become ourselves is"
     # generate_rest_of_the_text(co, prompt)
-
 
     classify(co)
 
